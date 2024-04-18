b23f73fa391917be6a474d59b81d98ee41008374
kristian <emailprogramador2@entornos.com>
2024-04-18 17:52:39 +0200

    Tercer commit desde el tercer Workspace por Kristian.
    Modificaciones:
    
    - Nueva clase "Luego"
    - Llamada a clase luego desde main

--- EntornosGitHub/src/principal/Principal.java
+++ EntornosGitHub/src/principal/Principal.java
@@ -5,8 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		Hola h = new Hola ();
 		Mundo m = new Mundo ();
+		Luego l = new Luego ();
 		
-		System.out.println(h.toString() + m.toString());
+		System.out.println(h.toString() + m.toString() + l.toString());
 
 	}
 
